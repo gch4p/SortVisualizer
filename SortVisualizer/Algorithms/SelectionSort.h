e367eb5d1d5579b7e5ba3098d17c560f98db41d0
gch4p <gpc0008@auburn.edu>
2022-10-21 07:37:22 -0500

    added selectionsort

---
+++ SortVisualizer/Algorithms/SelectionSort.h
@@ -0,0 +1,24 @@
+#ifndef SELECTIONSORT
+#define SELECTIONSORT
+
+#include "../SortAlgorithm.h"
+
+class SelectionSort : public SortAlgorithm {
+	void algo() override {
+		int min_idx = 0;
+		for (int i = 0; i < (length-1); ++i) {
+			min_idx = i;
+			for (int j = (i+1); j < length; ++j) {
+				setCursorPos(j);
+				if (numbers[j] < numbers[min_idx])
+					min_idx = j;
+
+			}
+			if (min_idx != i)
+				std::swap(numbers[i], numbers[min_idx]);
+		}
+		m_finished = 1;
+	}
+};
+
+#endif
